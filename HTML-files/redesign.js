98e0ce9b3d85d3db1d042fdcecfc5e0d14230684
Dom <dchwirot01@gmail.com>
2026-03-02 12:47:26 +0000

    added placeholder function for search

--- HTML-files/redesign.js
+++ HTML-files/redesign.js
@@ -3,9 +3,6 @@ let videos = Array.from(grid.children);
 //selecting the grid and making an array of its children (the video divs)
 
 document.addEventListener("DOMContentLoaded", () => {
-  let grid = document.querySelector(".videoGrid"); //select all divs with class "videoGrid" and puts them into grid var
-  let videos = Array.from(grid.children); //makes an array of the children of grid (the video divs)
-
   function shuffle(array) {
     for (var i = array.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
@@ -35,3 +32,5 @@ function closeVideo() {
   playerVideo.pause();
   playerVideo.src = "";
 }
+
+function searchItems() {}
